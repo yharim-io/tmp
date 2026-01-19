08382f38055ffda53e2eac643285bef248bd1f5c
yharim-io <yharim.io@gmail.com>
2026-01-20 02:34:50 +0800

    tmp

--- upcap/layer/attention.py
+++ upcap/layer/attention.py
@@ -5,19 +5,25 @@ class ConceptAttention(nn.Module):
 	
 	def __init__(self, dim: int):
 		super().__init__()
-		self.q_proj = nn.Linear(dim, dim, bias=False)
-		self.k_proj = nn.Linear(dim, dim, bias=False)
-		self.v_proj = nn.Linear(dim, dim, bias=False)
+		# self.q_proj = nn.Linear(dim, dim, bias=False)
+		# self.k_proj = nn.Linear(dim, dim, bias=False)
+		# self.v_proj = nn.Linear(dim, dim, bias=False)
 		self.scale = dim ** -0.5
 		
 	def forward(self, text_concepts: Tensor, concepts_feat: Tensor) -> Tensor:
-		Q: Tensor = self.q_proj(text_concepts)
-		K: Tensor = self.k_proj(concepts_feat)
-		V: Tensor = self.v_proj(concepts_feat)
+		# Q: Tensor = self.q_proj(text_concepts)
+		# K: Tensor = self.k_proj(concepts_feat)
+		# V: Tensor = self.v_proj(concepts_feat)
 		
-		attn_score: Tensor = (Q @ K.transpose(-2, -1)) * self.scale
+		# attn_score: Tensor = (Q @ K.transpose(-2, -1)) * self.scale
+		# attn_probs: Tensor = attn_score.softmax(dim=-1)
+		
+		# o: Tensor = attn_probs @ V
+		
+		# return o
+		
+		attn_score: Tensor = (text_concepts @ concepts_feat.transpose(-2, -1)) * self.scale
 		attn_probs: Tensor = attn_score.softmax(dim=-1)
-		
-		o: Tensor = attn_probs @ V
+		o = attn_probs @ concepts_feat
 		
 		return o
